164e130ccf6d7107634bbd9f689d9dd4bda27bcc
jakubmanczak <jakub@manczak.net>
2025-03-03 23:22:11 +0100

    2025-03-03 (zajęcia 1)

---
+++ 2025-03-03--01.py
@@ -0,0 +1,16 @@
+print("cokolwiek")
+print(2+7)
+print("miałem\nwpisać\tcokolwiek\ttestując\ntooo")
+print("won't work\a\twill trigger a visual bell instead")
+print("\"here be escaping characters\", you can use them henceforth \\ \\\n")
+
+zmienna = 10
+print("zmienna:", zmienna)
+print(f"zmienna: {zmienna}")
+
+print(zmienna + 1)
+# print(zmienna + "1")
+
+print("Ile masz lat?")
+wiek = input()
+print(f"Masz {wiek} lat.")
